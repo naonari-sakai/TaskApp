afc74fc25d736760676265bfc90d49585235d99d
naonari-sakai <sakai.n.w@gmail.com>
2020-08-23 11:14:22 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/sakai/naonari/taskapp/Task.kt
@@ -0,0 +1,17 @@
+package jp.techacademy.sakai.naonari.taskapp
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.io.Serializable
+import java.util.*
+
+open class Task : RealmObject(), Serializable {
+    //Realmが内部的にTaskを継承したクラスを作成するためopen
+    var title: String = ""
+    var contents: String = ""
+    var date: Date = Date()
+
+    //idをプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
+}
